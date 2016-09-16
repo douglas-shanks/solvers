3213a661842864ac6e27b76132f292fa49d3d5f4
douglas-shanks <dougysh@gmail.com>
2016-09-16 14:35:35 +0100

    Update 2norm.f90

--- diffusion_solver/1D/2norm.f90
+++ diffusion_solver/1D/2norm.f90
@@ -1,4 +1,5 @@
 subroutine norm(error,U1,U2,Nt,Nx)
+! compute the 2-norm of the error = U1-U2
 
 integer, intent(in) :: Nx, Nt 
 real(kind=8), intent(in), dimension(Nx) :: U1
@@ -7,9 +8,7 @@ real(kind=8), intent(out) :: error
 
 error = 0
     do i=1,Nx
-    
     	  error = error + SQRT((U1(i)- U2(Nt,i))**2)
-    	  
     enddo
 
 end soubroutine norm
